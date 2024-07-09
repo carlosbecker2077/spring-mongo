1d20dea6a7a7d04431fbcb4447b49f9d85da22b5
Carlos Becker <carlos.lemos@dataside.com.br>
2024-07-09 11:34:48 -0300

    user repository + service and connecting to mongo

--- src/main/java/com/springmongo/springmongo/SpringmongoApplication.java
+++ src/main/java/com/springmongo/springmongo/SpringmongoApplication.java
@@ -3,7 +3,7 @@ package com.springmongo.springmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SpringmongoApplication {
 
 	public static void main(String[] args) {

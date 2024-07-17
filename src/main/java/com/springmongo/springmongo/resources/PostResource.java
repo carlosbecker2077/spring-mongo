ead62d85e2b8b8b8fda9eb503452190de9867c1b
Carlos Becker <carlos.lemos@dataside.com.br>
2024-07-17 17:45:35 -0300

    simple query with query methods

--- src/main/java/com/springmongo/springmongo/resources/PostResource.java
+++ src/main/java/com/springmongo/springmongo/resources/PostResource.java
@@ -1,9 +1,13 @@
 package com.springmongo.springmongo.resources;
 
 import com.springmongo.springmongo.domain.Post;
+import com.springmongo.springmongo.resources.util.URL;
 import com.springmongo.springmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
@@ -14,7 +18,13 @@ public class PostResource {
     @GetMapping(value = "/{id}")
     @ResponseBody
     public Post findById(@PathVariable String id) {
-        Post post = this.postService.findById(id);
-        return post;
+        return this.postService.findById(id);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    @ResponseBody
+    public List<Post> findByTitle(@RequestParam(value = "title", defaultValue = "") String title) {
+        title = URL.decodeParam(title);
+        return this.postService.findByTitle(title);
     }
 }
--- src/main/java/com/springmongo/springmongo/services/PostService.java
+++ src/main/java/com/springmongo/springmongo/services/PostService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -22,5 +21,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+
+    public List<Post> findByTitle(String title) {
+        return this.postRepository.findByTitleContaining(title);
     }
 }
